da52fb05a1987c9c022af0312edd122295f908c1
王佳诺 <jianuo.wang@wedcm.com>
2020-07-23 14:00:35 +0800

    one

---
+++ exercise/src/main/java/com/wedcm/exercise01/App.java
@@ -0,0 +1,13 @@
+package com.wedcm.exercise01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
